3054e4002cb81049832c8d9311b648883ac5a8d9
Sean Cui <781583635@qq.com>
2025-01-24 08:42:23 +0800

    code optimization

--- src/main/java/com/volvo/emspdemo/util/Base36Util.java
+++ src/main/java/com/volvo/emspdemo/util/Base36Util.java
@@ -21,4 +21,31 @@ public class Base36Util {
 
         return result.toString();
     }
+
+    /**
+     * 将字符格式化为指定长度，不足部分用零填充
+     *
+     * @param input  需要格式化的字符
+     * @param length 输出字符串的总长度
+     * @return 格式化后的字符串
+     */
+    public static String formatString(String input, int length) {
+        if (input == null) {
+            throw new IllegalArgumentException("输入字符串不能为 null");
+        }
+
+        // 如果输入字符串长度大于等于指定长度，直接返回原字符串
+        if (input.length() >= length) {
+            return input;
+        }
+
+        // 计算需要填充的零的数量
+        int zerosToAdd = length - input.length();
+
+        // 生成填充的零
+        String zeros = "0".repeat(zerosToAdd);
+
+        // 返回填充后的字符串
+        return zeros + input;
+    }
 }
